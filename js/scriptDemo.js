c02b862871b6ea192b9efbdedae83f7f1df45067
MathisGrondin <grondin.mathis@hotmail.com>
2023-05-25 14:00:42 -0400

    commit pour aout page demo pour ssj

---
+++ js/scriptDemo.js
@@ -0,0 +1,84 @@
+
+// Retourne 1 élément
+let h1PL = document.getElementById('h1PL');
+let TexteBtn = document.getElementById('TexteBtn');
+let milieu = document.getElementById('milieu');
+let bgSW = document.getElementById("bgSW");
+let hrOrange = document.getElementsByTagName("hr");
+let texteAccueil = document.getElementById("texteaccueil");
+let TexteSec = document.getElementById("TexteSec");
+let TexteMarket = document.getElementById("texteMarket");
+let btnMail = document.getElementById("btnMail");
+let titre3 = document.getElementById("Titre3");
+let logo1 = document.getElementById("logo1");
+let logo2 = document.getElementById("logo2");
+let logo3 = document.getElementById("logo3");
+let logo4 = document.getElementById("logo4");
+let h4L1 = document.getElementById("h4L1");
+let h4L2 = document.getElementById("h4L2");
+let h4L3 = document.getElementById("h4L3");
+let h4L4 = document.getElementById("h4L4");
+let pL1 = document.getElementById("pL1"); 
+let pL2 = document.getElementById("pL2");
+let pL3 = document.getElementById("pL3");
+let pL4 = document.getElementById("pL4");
+let imgB1 = document.getElementById("imgB1");
+let imgB2 = document.getElementById("imgB2");
+let imgB3 = document.getElementById("imgB3");
+let imgB4 = document.getElementById("imgB4");
+let imgB5 = document.getElementById("imgB5");
+let imgB6 = document.getElementById("imgB6");
+let divImages = document.getElementById("divImages");
+let rowImage = document.getElementById("rowImage");
+
+// retourne plusieurs éléments (tableaux)
+// let row = document.getElementsByClassName('row');
+// console.log(row);
+
+// modifier entre deux balises
+// h1PL.innerHTML = "Ville de trois-Rivières";
+// TexteBtn.innerHTML = "Y aller";
+// TexteSec.innerHTML = "Une ville à couper le souffle !";
+// TexteMarket.innerHTML = "Effectivement, le CAP est une des meilleures place où vivre à 3R !";
+// TexteBtn.setAttribute("href", "https://www.google.com/maps/place/Trois-Rivi%C3%A8res,+QC/@46.3729169,-72.7358727,11z/data=!3m1!4b1!4m5!3m4!1s0x4cc7c8cfcf1f467d:0xb9d6b0325e28c3d2!8m2!3d46.342982!4d-72.5420716");
+// btnMail.setAttribute("href", "mailto:mathis.grondin.01@edu.cegeptr.qc.ca");
+// btnMail.innerHTML = "Besoin d'en parler ?";
+// titre3.innerHTML = "Une ville étonnante";
+
+// .style accède au CSS de notre objet!
+// bgSW.style.backgroundImage= 'url("../img/v3rbanniere.jpg")';
+// milieu.style.backgroundColor = "pink";
+// hrOrange[0].style.border = '3px solid purple';
+// hrOrange[0].style.maxWidth = '900px';
+// hrOrange[1].style.border = '3px solid purple';
+// hrOrange[1].style.maxWidth = '900px';
+// hrOrange[2].style.border = '3px solid purple';
+// hrOrange[2].style.maxWidth = '900px';
+// hrOrange[3].style.border = '3px solid purple';
+// hrOrange[3].style.maxWidth = '900px';
+// h1PL.style.setProperty("color", "black", "important");
+// TexteSec.style.setProperty("color", "orange", "important");
+// texteAccueil.style.setProperty("color", "lime", "important");
+
+// logo1.setAttribute("class", "fa-solid fa-droplet fa-3x my-5 mx-5");
+// h4L1.innerHTML = "Prix de l'eau";
+// pL1.innerHTML = "0$ :)";
+
+// logo2.setAttribute("class", "fa-solid fa-magnifying-glass fa-3x my-5 mx-5");
+// h4L2.innerHTML = "Découvert par";
+// pL2.innerHTML = "Laviolette";
+
+// logo3.setAttribute("class", "fa-solid fa-hourglass fa-3x my-5 mx-5");
+// h4L3.innerHTML = "Année création";
+// pL3.innerHTML = "1634";
+
+// logo4.setAttribute("class", "fa-solid fa-person fa-3x my-5 mx-5");
+// h4L4.innerHTML = "Nombre d'habitants";
+// pL4.innerHTML = "139 163 habitants";
+
+// imgB1.style.backgroundImage = 'url("./img/v3r1.jpg")';
+// imgB2.style.backgroundImage = 'url("./img/v3r2.jpeg")';
+// imgB3.style.backgroundImage = 'url("./img/v3r3.jpg")';
+// imgB4.style.backgroundImage = 'url("./img/v3r4.jpg")';
+// imgB5.style.backgroundImage = 'url("./img/v3r5.jpg")';
+// imgB6.style.backgroundImage = 'url("./img/v3r6.jpg")';
